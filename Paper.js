bf0ff941122fd5ebae937a8cc7d20dd96886dd37
PaavaniAgarwal <84220794+PaavaniAgarwal@users.noreply.github.com>
2021-06-10 00:17:33 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,51 @@
+class Paper{
+	constructor(){
+      var options={
+		  isStatic:false,
+		  restitution:0.3,
+		  friction:0,
+		  density:1.2
+	  }
+		
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			angleMode(RADIANS)
+			fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+			
+	}
+
+}
